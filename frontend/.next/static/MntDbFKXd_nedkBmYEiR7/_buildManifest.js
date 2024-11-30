42e347b4aefa59432c1b7e82e6d1ab89b9922d4c
Naoto Onoda <onoda@rakudasystem.co.jp>
2024-11-30 09:52:27 +0900

    Add: フロントエンドのビルドファイル

--- frontend/.next/static/MntDbFKXd_nedkBmYEiR7/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-b6b383c5bc6a87ff.js"],"/_error":["static/chunks/pages/_error-7ba65e1336b92748.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
